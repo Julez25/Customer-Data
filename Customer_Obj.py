3bb5c01165373ae6be332e0db403cafa59b2501b
Julez25 <julian0205.jt@gmail.com>
2025-02-19 13:56:20 +0100

    Erster Commit

---
+++ Customer_Obj.py
@@ -0,0 +1,27 @@
+import csv
+class NewCust():
+    
+    def __init__(self, vorname_, nachname_, adresse_, auftrag_):
+        self.vorname_ = vorname_
+        self.nachname_ = nachname_
+        self.adresse_ = adresse_
+        self.auftrag_ = auftrag_
+    
+    def saveData(self):
+        with open("Customer_List.csv", mode="w", newline="", encoding="utf-8") as file:
+            dict_Data = {'Vorname ',
+                     'Nachname ', 
+                     'Adresse ',
+                     'Auftrag '}
+            saver_ = csv.writer(file)
+            saver_.writerow(dict_Data)
+    
+    def getData(self):
+        with open("Customer_List.csv", mode="r", newline="", encoding="utf-8") as file:
+            getter_ = csv.reader(file)
+            for row in getter_:
+                print(row)
+CustObj = NewCust("julian", "Tomic", "blabalbla", "quitsche ente")
+CustObj.saveData()
+CustObj.getData()
+
