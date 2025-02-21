f0c157fc46760cab74b055fd02877d9f5ca73546
Julez25 <julian0205.jt@gmail.com>
2025-02-21 16:09:41 +0100

    Created via Code and SQLite a Database Script, where the new User is saved

--- Data_Base/Customer_Obj.py
+++ Data_Base/Customer_Obj.py
@@ -1,7 +1,7 @@
 import random
 class NewCust():
     
-    def __init__(self, id_Num_, id_Order_, first_Name_, sec_Name_, land_, region_, city_, plz_, adress_, auftrag_):
+    def __init__(self, id_Num_, id_Order_, first_Name_, sec_Name_, land_, region_, city_, plz_, adress_):
         self.id_Num_ = id_Num_
         self.id_Order_ = id_Order_
         self.first_Name_ = first_Name_
@@ -9,11 +9,10 @@ class NewCust():
         self.land_ = land_
         self.region_ = region_
         self.city_ = city_
-        self.plz = plz_
+        self.plz_ = plz_
         self.adress_ = adress_
-        self.auftrag_ = auftrag_
     
-    def get_Id_num(self):
+    def get_Id_Num(self):
         return self.id_Num_
     def get_Id_Order(self):
         return self.id_Order_
@@ -28,7 +27,7 @@ class NewCust():
     def get_City(self):
         return self.city_
     def get_PLZ(self):
-        return self.plz
+        return self.plz_
     def get_Adress(self):
         return self.adress_
     
--- Data_Base/Main.py
+++ Data_Base/Main.py
@@ -1,6 +1,38 @@
 import sqlite3
 import Customer_Obj
 
+#Daten durch die NewCust Class in die DataBase einfügen
+def saveCust():
+    cursor.execute("""
+    INSERT INTO Users (
+    id, 
+    order_id, 
+    first_N, 
+    second_N, 
+    land, 
+    region, 
+    city, 
+    plz, 
+    adress) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""", 
+    (newCust.get_Id_Num(),
+     newCust.get_Id_Order(),
+     newCust.get_First_Name(),
+     newCust.get_Sec_Name(),
+     newCust.get_Land(),
+     newCust.get_Region(),
+     newCust.get_City(),
+     newCust.get_PLZ(),
+     newCust.get_Adress())
+     )
+    conn.commit()
+
+def getData():
+    cursor.execute("SELECT * FROM Users")
+    rows = cursor.fetchall()  # Holt alle Ergebnisse aus der Abfrage
+
+    for row in rows:
+        print(row)
 #Verbindung zur Datenbank herstellen
 conn = sqlite3.connect("Cust_Database.db") 
 cursor = conn.cursor() #Cursor Objekt
@@ -8,25 +40,25 @@ cursor = conn.cursor() #Cursor Objekt
 #Tabelle mit Kundendaten erstellen
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    order_id INTEGER
+    id INTEGER,
+    order_id INTEGER,
     first_N TEXT NOT NULL,
     second_N TEXT NOT NULL,
     land TEXT NOT NULL,
     region TEXT NOT NULL,
     city TEXT NOT NULL,
-    plz INTEGER,
-               
-    adress TEXT NOT NULL,
-    
+    plz INTEGER,               
+    adress TEXT NOT NULL    
 )
 """)
 conn.commit()
 
-#Daten durch die Customer Class in die DataBase einfügen
-def saveCust():
-    cursor.execute("INSERT INTO users (id, order_id, first_N, second_N, land, region, city, plz, adress) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", ())
-    
-newCust = Customer_Obj(123, 200, "julian", "T", "DE", "By", "MU", 80689, "blabla")
+newCust = Customer_Obj.NewCust(123, 200, "julian", "T", "DE", "By", "MU", 80689, "blabla")
 saveCust()
+getData()
+cursor.close()
+conn.close()
+
+    
+
 
